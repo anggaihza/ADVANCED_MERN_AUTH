634a31c41b0c8dbfee1883c2904bb5676a2cf376
Asep Angga Ihza S <angga.asep98@gmail.com>
2025-04-21 17:45:54 +0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv"
+
+import { connectDB } from "./db/connectDB.js";
+
+import authRoutes from "./routes/auth.route.js"
+
+dotenv.config();
+
+const app = express();
+const PORT = 4000
+
+app.use(express.json()) // allows us to access req.body
+
+app.use("/api/auth", authRoutes)
+
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// pass Mongo Esemkaencjl123
